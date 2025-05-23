d65b6d988a4e41b0386d44807386591ec7c7c7c1
beyersdorfertj <beyersdorfertj@ldschurch.org>
2025-05-23 23:44:40 +0200

    push origin main

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,21 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+class Program {
+    static void Main(string[] args) {
+        Resume myResume = new Resume();
+        myResume._name = "Jörg Beyersdorfer";
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+        myResume._jobs.Add(job1);
+        Job job2 = new Job();
+        job2._jobTitle = "Database Engineer";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+        myResume._jobs.Add(job2);
+        myResume.Display();
     }
 }
