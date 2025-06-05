b1eae18938ab1040807346c9bfa78762d0aa9e30
beyersdorfertj <beyersdorfertj@ldschurch.org>
2025-06-05 22:30:41 +0200

    W05 Learning Activity: Inheritance

---
+++ week05/Homework/MathAssignement.cs
@@ -0,0 +1,13 @@
+class MathAssignment : Assignment {
+    private string _textbookSection;
+    private string _problems;
+
+    public MathAssignment(string studentName, string topic, string textbookSection, string problems) : base(studentName, topic) {
+        this._textbookSection = textbookSection;
+        this._problems = problems;
+    }
+
+    public string GetHomeworkList() {
+        return $"Textbook Section {this._textbookSection} Problems {this._problems}";
+    }
+}
--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,20 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine();
+
+        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("SRoberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine();
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II by Mary Waters");
+        Console.WriteLine();
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
